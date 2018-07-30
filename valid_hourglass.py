0dedaf500c7f65d4d42e065eba3304eb0af76b01
nulledge <the.nulledge@gmail.com>
2018-07-30 16:46:45 +0900

    Change argmax with softargmax in the hourglass validation script

--- valid_hourglass.py
+++ valid_hourglass.py
@@ -59,59 +59,26 @@ with tqdm(total=len(data), desc='%d epoch' % pretrained_epoch) as progress:
     with torch.set_grad_enabled(False):
         for images, _, keypoints in data:
             images_cpu = images
-            images = images.to(device)
+            images_device = images.to(device)
 
-            outputs = hourglass(images)
+            outputs = hourglass(images_device)
+            outputs = outputs[-1]  # Heatmaps from the last stack in batch-channel-height-width shape.
 
-            heatmaps = merge_to_color_heatmap(outputs[-1].data)
-            heatmaps = heatmaps.permute(0, 2, 3, 1).cpu()  # NHWC
-
-            resized_heatmaps = list()
-            for idx, ht in enumerate(heatmaps):
-                color_ht = skimage.transform.resize(ht.numpy(), (256, 256), mode='constant')
-                resized_heatmaps.append(color_ht.transpose(2, 0, 1)) # CHW
-
-            resized_heatmaps = np.stack(resized_heatmaps, axis=0)
-
-            overlayed_image = np.clip(np.asarray(images.data) * 0.6 + resized_heatmaps * 0.4, 0, 1.)
-
-            for idx, image in enumerate(overlayed_image):
-                imageio.imwrite('prediction_{idx}.jpg'.format(idx=idx), image.transpose(1, 2, 0))
-
-            outputs = outputs[-1]
             n_batch = outputs.shape[0]
             total = total + n_batch
 
-            poses_2D = torch.zeros(n_batch, 16, 2)
+            poses_2D = torch.zeros(n_batch, 16, 2)  # MPII has 16 joints.
             for batch in range(n_batch):
                 image_soft = np.asarray(images[batch].data)
-                image_hard = np.asarray(images[batch].data)
                 for joint, heatmap in enumerate(outputs[batch]):
-                    poses_2D[batch][joint] = softargmax(heatmap)
+                    poses_2D[batch, joint, :] = softargmax(heatmap)
 
-                    for tx in range(-5, 5):
-                        for ty in range(-5, 5):
-                            image_soft[:, int(poses_2D[batch][joint][1])*4 + ty, int(poses_2D[batch][joint][0])*4 + tx] = [1, 0, 0]
+                diff = poses_2D.data[batch] - keypoints[batch]  # shape=(16, 2)
+                dist = torch.sqrt(diff[:, 0] ** 2 + diff[:, 1] ** 2)  # shape=(16)
 
-                    pose = int(torch.argmax(heatmap.view(-1)).data)
-                    poses_2D[batch][joint][:] = torch.Tensor([pose % 64, pose // 64])
-
-                    for tx in range(-5, 5):
-                        for ty in range(-5, 5):
-                            image_hard[:, int(poses_2D[batch][joint][1])*4 + ty, int(poses_2D[batch][joint][0])*4 + tx] = [1, 0, 0]
-                imageio.imwrite('soft_argmax{idx}.jpg'.format(idx=batch), image_soft.transpose(1, 2, 0))
-                imageio.imwrite('argmax{idx}.jpg'.format(idx=batch), image_hard.transpose(1, 2, 0))
-
-                # diff = poses_2D.data[batch] - keypoints[batch]  # shape=(16, 2)
-                # dist = torch.sqrt(diff[:, 0] ** 2 + diff[:, 1] ** 2)  # shape=(16)
-                #
-                # for joint in torch.nonzero(torch.le(dist, 64 * 0.1)):
-                #     hit[joint] = hit[joint] + 1
+                for joint in torch.nonzero(torch.le(dist, 64 * 0.1)):
+                    hit[joint] = hit[joint] + 1
 
             progress.update(1)
 
-            break
-
-print(total)
-print(hit)
 print(hit / total * 100)  # In percentage.
