Fix bug on the data normalization pipeline

H36M/data.py
@@ -45,7 +45,6 @@ class Dataset(torch_data.Dataset):
             image, heatmap = [-1, -1]
 
         for dim, anno in zip([2, 3], [Annotation.Part, Annotation.S]):
-            data[anno] = data[anno] - data[anno][0]  # root-centered
             data[anno] = (data[anno] - self.mean[dim]) / self.stddev[dim]  # normalize for each joint and coord.
             data[anno] = np.asarray(data[anno], dtype=np.float32)
 
@@ -100,8 +99,13 @@ class Dataset(torch_data.Dataset):
             anno = Annotation.S
         else:
             anno = Annotation.Part
-        data = np.reshape(np.asarray(self.data[self.task][anno]), newshape=(-1, dim * 17))
-        mean = np.reshape(np.mean(data, axis=0), newshape=(-1, dim))
-        stddev = np.reshape(np.std(data, axis=0), newshape=(-1, dim))
+
+        root = self.data[self.task][anno][:][0]  # Frame-Dim
+        root_centered = self.data[self.task][anno] - root  # Frame-Joint-Dim
+        self.data[self.task][anno] = root_centered
+
+        data = np.reshape(np.asarray(self.data[self.task][anno]), newshape=(-1, dim * 17))  # Frame-Dim*Joint
+        mean = np.reshape(np.mean(data, axis=0), newshape=(17, dim))  # Joint-Dim
+        stddev = np.reshape(np.std(data, axis=0), newshape=(17, dim))  # Joint-Dim
 
         return mean, stddev

Model/bilinear.py
@@ -18,8 +18,11 @@ class BilinearUnit(nn.Module):
         super().__init__()
 
         self.encode = heavy_linear(in_features=2 * 17, out_features=1024)
-        self.linear = nn.ModuleList([
-            heavy_linear(in_features=1024, out_features=1024) for _ in range(2)
+        self.bilinear = nn.ModuleList([
+            nn.Sequential(
+                heavy_linear(in_features=1024, out_features=1024),
+                heavy_linear(in_features=1024, out_features=1024),
+            ) for _ in range(2)
         ])
         self.decode = nn.Linear(in_features=1024, out_features=3 * 17, bias=True)
 
@@ -27,9 +30,9 @@ class BilinearUnit(nn.Module):
         out_tensor = in_tensor
 
         out_tensor = self.encode(out_tensor)
-        for linear in self.linear:
+        for bilinear in self.bilinear:
             skip_tensor = out_tensor
-            out_tensor = linear(out_tensor)
+            out_tensor = bilinear(out_tensor)
             out_tensor = out_tensor + skip_tensor
         out_tensor = self.decode(out_tensor)
 

train_bilinear.py
@@ -28,7 +28,7 @@ bilinear, optimizer, criterion, step, pretrained_epoch, lr = Model.bilinear.load
 loss_window = None
 windows = [loss_window]
 
-for epoch in range(pretrained_epoch + 1, pretrained_epoch + 100 + 1):
+for epoch in range(pretrained_epoch + 1, pretrained_epoch + 200 + 1):
     with tqdm(total=len(data), desc='%d epoch' % epoch) as progress:
 
         with torch.set_grad_enabled(True):