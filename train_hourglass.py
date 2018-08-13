51205891682be2995285dcb5f622dd414225dd0b
nulledge <the.nulledge@gmail.com>
2018-08-13 12:25:23 +0900

    Apply gradient norm and visualize batch norm mean and var

--- train_hourglass.py
+++ train_hourglass.py
@@ -1,12 +1,13 @@
 import numpy as np
 import torch
+import torch.nn as nn
+import imageio
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import MPII
 import Model.hourglass
-from util.path import safe_path
-from util.visualize import draw
+from util.visualize import draw, draw_line
 from util import config
 
 assert config.task == 'train'
@@ -26,7 +27,11 @@ device = torch.device(config.device)
 hourglass, optimizer, criterion, step, pretrained_epoch = Model.hourglass.load_model(device,
                                                                                      config.pretrained['hourglass'])
 
+hourglass.train()
+
 loss_window, gt_image_window, out_image_window = None, None, None
+max_mean_1_window, max_var_1_window = None, None
+max_mean_2_window, max_var_2_window = None, None
 windows = [loss_window, gt_image_window, out_image_window]
 
 for epoch in range(pretrained_epoch + 1, pretrained_epoch + 100 + 1):
@@ -45,7 +50,29 @@ for epoch in range(pretrained_epoch + 1, pretrained_epoch + 100 + 1):
                 loss = sum([criterion(output, heatmaps) for output in outputs])
                 loss.backward()
 
+                nn.utils.clip_grad_norm_(hourglass.parameters(), max_norm=1)
+
                 optimizer.step()
+
+                mean_and_var = [
+                    float(hourglass.hourglass[1].res.conv[0][0].running_mean.max()),
+                    float(hourglass.hourglass[1].res.conv[0][0].running_var.max()),
+                    float(hourglass.hourglass[7].upscale[0][0].conv[0][0].running_mean.max()),
+                    float(hourglass.hourglass[7].upscale[0][0].conv[0][0].running_var.max()),
+                ]
+
+                max_mean_1_window = draw_line(x=step,
+                                              y=np.array([mean_and_var[0]]),
+                                              window=max_mean_1_window)
+                max_var_1_window = draw_line(x=step,
+                                             y=np.array([mean_and_var[1]]),
+                                             window=max_var_1_window)
+                max_mean_2_window = draw_line(x=step,
+                                              y=np.array([mean_and_var[2]]),
+                                              window=max_mean_2_window)
+                max_var_2_window = draw_line(x=step,
+                                             y=np.array([mean_and_var[3]]),
+                                             window=max_var_2_window)
 
                 if config.visualize:
                     windows = draw(step, loss, images_cpu, heatmaps, outputs, windows)
